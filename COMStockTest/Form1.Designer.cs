efbfdcf9af76e8bc086e3b1c9c79f79b84f184b0
VA1985 <vadim.ametov@tpv-tech.com>
2020-03-03 10:03:41 +0300

    Init

---
+++ COMStockTest/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace COMStockTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SerialNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OrderNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openport = new System.Windows.Forms.Button();
+            this.cbport = new System.Windows.Forms.ComboBox();
+            this.cbrate = new System.Windows.Forms.ComboBox();
+            this.cbparity = new System.Windows.Forms.ComboBox();
+            this.cbstop = new System.Windows.Forms.ComboBox();
+            this.cbdata = new System.Windows.Forms.ComboBox();
+            this.tblog = new System.Windows.Forms.TextBox();
+            this.cbport1 = new System.Windows.Forms.ComboBox();
+            this.tblog2 = new System.Windows.Forms.TextBox();
+            this.tb_palletqty = new System.Windows.Forms.TextBox();
+            this.Palletqty = new System.Windows.Forms.Label();
+            this.lb_pos = new System.Windows.Forms.Label();
+            this.bt_refresh = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SerialNumber,
+            this.OrderNo});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(399, 469);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // SerialNumber
+            // 
+            this.SerialNumber.HeaderText = "Serial Number";
+            this.SerialNumber.Name = "SerialNumber";
+            this.SerialNumber.Width = 205;
+            // 
+            // OrderNo
+            // 
+            this.OrderNo.HeaderText = "OrderNo";
+            this.OrderNo.Name = "OrderNo";
+            this.OrderNo.ReadOnly = true;
+            this.OrderNo.Width = 150;
+            // 
+            // openport
+            // 
+            this.openport.Location = new System.Drawing.Point(440, 217);
+            this.openport.Name = "openport";
+            this.openport.Size = new System.Drawing.Size(121, 23);
+            this.openport.TabIndex = 2;
+            this.openport.Text = "Открыть порт";
+            this.openport.UseVisualStyleBackColor = true;
+            this.openport.Click += new System.EventHandler(this.openport_Click);
+            // 
+            // cbport
+            // 
+            this.cbport.FormattingEnabled = true;
+            this.cbport.Location = new System.Drawing.Point(440, 13);
+            this.cbport.Name = "cbport";
+            this.cbport.Size = new System.Drawing.Size(121, 21);
+            this.cbport.TabIndex = 3;
+            // 
+            // cbrate
+            // 
+            this.cbrate.FormattingEnabled = true;
+            this.cbrate.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "28800",
+            "36000",
+            "115000"});
+            this.cbrate.Location = new System.Drawing.Point(440, 50);
+            this.cbrate.Name = "cbrate";
+            this.cbrate.Size = new System.Drawing.Size(121, 21);
+            this.cbrate.TabIndex = 4;
+            // 
+            // cbparity
+            // 
+            this.cbparity.FormattingEnabled = true;
+            this.cbparity.Location = new System.Drawing.Point(440, 92);
+            this.cbparity.Name = "cbparity";
+            this.cbparity.Size = new System.Drawing.Size(121, 21);
+            this.cbparity.TabIndex = 5;
+            // 
+            // cbstop
+            // 
+            this.cbstop.FormattingEnabled = true;
+            this.cbstop.Location = new System.Drawing.Point(440, 129);
+            this.cbstop.Name = "cbstop";
+            this.cbstop.Size = new System.Drawing.Size(121, 21);
+            this.cbstop.TabIndex = 6;
+            // 
+            // cbdata
+            // 
+            this.cbdata.FormattingEnabled = true;
+            this.cbdata.Items.AddRange(new object[] {
+            "7",
+            "8",
+            "9"});
+            this.cbdata.Location = new System.Drawing.Point(440, 172);
+            this.cbdata.Name = "cbdata";
+            this.cbdata.Size = new System.Drawing.Size(121, 21);
+            this.cbdata.TabIndex = 7;
+            // 
+            // tblog
+            // 
+            this.tblog.Location = new System.Drawing.Point(440, 506);
+            this.tblog.Name = "tblog";
+            this.tblog.ReadOnly = true;
+            this.tblog.Size = new System.Drawing.Size(248, 21);
+            this.tblog.TabIndex = 14;
+            // 
+            // cbport1
+            // 
+            this.cbport1.FormattingEnabled = true;
+            this.cbport1.Location = new System.Drawing.Point(567, 13);
+            this.cbport1.Name = "cbport1";
+            this.cbport1.Size = new System.Drawing.Size(121, 21);
+            this.cbport1.TabIndex = 15;
+            // 
+            // tblog2
+            // 
+            this.tblog2.Location = new System.Drawing.Point(12, 506);
+            this.tblog2.Name = "tblog2";
+            this.tblog2.ReadOnly = true;
+            this.tblog2.Size = new System.Drawing.Size(399, 21);
+            this.tblog2.TabIndex = 16;
+            // 
+            // tb_palletqty
+            // 
+            this.tb_palletqty.Location = new System.Drawing.Point(630, 289);
+            this.tb_palletqty.Name = "tb_palletqty";
+            this.tb_palletqty.ReadOnly = true;
+            this.tb_palletqty.Size = new System.Drawing.Size(58, 21);
+            this.tb_palletqty.TabIndex = 17;
+            // 
+            // Palletqty
+            // 
+            this.Palletqty.AutoSize = true;
+            this.Palletqty.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Palletqty.Location = new System.Drawing.Point(437, 294);
+            this.Palletqty.Name = "Palletqty";
+            this.Palletqty.Size = new System.Drawing.Size(177, 16);
+            this.Palletqty.TabIndex = 18;
+            this.Palletqty.Text = "Recommended pallet quantity";
+            // 
+            // lb_pos
+            // 
+            this.lb_pos.AutoSize = true;
+            this.lb_pos.Location = new System.Drawing.Point(372, 490);
+            this.lb_pos.Name = "lb_pos";
+            this.lb_pos.Size = new System.Drawing.Size(46, 13);
+            this.lb_pos.TabIndex = 19;
+            this.lb_pos.Text = "Кол-во:";
+            // 
+            // bt_refresh
+            // 
+            this.bt_refresh.Location = new System.Drawing.Point(440, 258);
+            this.bt_refresh.Name = "bt_refresh";
+            this.bt_refresh.Size = new System.Drawing.Size(121, 23);
+            this.bt_refresh.TabIndex = 20;
+            this.bt_refresh.Text = "Обновить порты";
+            this.bt_refresh.UseVisualStyleBackColor = true;
+            this.bt_refresh.Click += new System.EventHandler(this.bt_refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 538);
+            this.Controls.Add(this.bt_refresh);
+            this.Controls.Add(this.lb_pos);
+            this.Controls.Add(this.Palletqty);
+            this.Controls.Add(this.tb_palletqty);
+            this.Controls.Add(this.tblog2);
+            this.Controls.Add(this.cbport1);
+            this.Controls.Add(this.tblog);
+            this.Controls.Add(this.cbdata);
+            this.Controls.Add(this.cbstop);
+            this.Controls.Add(this.cbparity);
+            this.Controls.Add(this.cbrate);
+            this.Controls.Add(this.cbport);
+            this.Controls.Add(this.openport);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button openport;
+        private System.Windows.Forms.ComboBox cbport;
+        private System.Windows.Forms.ComboBox cbrate;
+        private System.Windows.Forms.ComboBox cbparity;
+        private System.Windows.Forms.ComboBox cbstop;
+        private System.Windows.Forms.ComboBox cbdata;
+        private System.Windows.Forms.ComboBox cbport1;
+        public System.Windows.Forms.TextBox tblog;
+        private System.Windows.Forms.TextBox tblog2;
+        private System.Windows.Forms.TextBox tb_palletqty;
+        private System.Windows.Forms.Label Palletqty;
+        private System.Windows.Forms.Label lb_pos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SerialNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OrderNo;
+        private System.Windows.Forms.Button bt_refresh;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ COMStockTest/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Data.OleDb;
+using DevExpress.XtraGrid;
+using System.Drawing.Printing;
+using System.Linq;
+
+namespace COMStockTest
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {//поменять, чтобы в программе выбирались номера портов сканеров и их открывать
+
+      
+       //public SerialPort Port = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+        //public SerialPort Port1 = new SerialPort("COM2", 9600, Parity.None, 8, StopBits.One);
+        private const string connectionString =
+            "Provider=OraOLEDB.Oracle.1;Password=adminmanager;Persist Security Info=True;User ID=sfis;Data Source=SFIS_TEST";
+        private const string connectionStringStock =
+            "Provider=SQLOLEDB.1;Password=5tgbnji9;Persist Security Info=True;User ID=sa;Data Source=172.18.0.8;Connect Timeout = 3;Initial Catalog=tpvstock";
+        public OleDbConnection myOleDbConnection = new OleDbConnection(connectionString);
+        public OleDbCommand myOleDbCommand;
+        public OleDbConnection myOleDbConnectionStock = new OleDbConnection(connectionStringStock);
+        public OleDbCommand myOleDbCommandStock;
+     
+        public string _msg, _msg1, SN;
+        public int x;
+        delegate void SetTextCallback(string text);
+        public Form1()
+        {
+            InitializeComponent();
+            myOleDbConnection.Open();
+            myOleDbConnectionStock.Open();
+        }
+        private void Form1_load(object sender, EventArgs e)
+        {
+
+            //SetDefaults();
+            SetPortNameValues(cbport);
+            SetPortNameValues(cbport1);
+
+
+        }
+        public void SetPortNameValues(object obj)
+        {
+           
+
+            foreach (string str in SerialPort.GetPortNames())
+            {
+             
+                string com = cbport.Items.ToString();
+                //if (str != com)
+                ((ComboBox)obj).Items.Add(str);
+            }
+        }
+        //private void SetDefaults()
+        //{
+
+        //    cbport.SelectedIndex = 0;
+        //    cbrate.SelectedText = "9600";
+        //    cbparity.SelectedIndex = 0;
+        //    cbstop.SelectedIndex = 1;
+        //    cbdata.SelectedIndex = 1;
+        //}
+        public void comportmanager()
+        {
+            try
+            {
+                SerialPort Port = new SerialPort(cbport.Text, 9600, Parity.None, 8, StopBits.One);
+                SerialPort Port1 = new SerialPort(cbport1.Text, 9600, Parity.None, 8, StopBits.One);
+                if (Port.IsOpen == true) Port.Close();
+                if (Port1.IsOpen == true) Port1.Close();
+                //Port.BaudRate = int.Parse(cbrate.Text);    //BaudRate
+                //Port.DataBits = int.Parse(cbdata.Text);    //DataBits
+                //Port.StopBits = (StopBits)Enum.Parse(typeof(StopBits), cbstop.Text);    //StopBits
+                //Port.Parity = (Parity)Enum.Parse(typeof(Parity), cbparity.Text);    //Parity
+                //Port.PortName = cbport.Text;   //PortName
+                //                                //now open the port
+                
+                Port.Open();
+                Port1.Open();
+                tblog.Text = "Порты открыты";
+                Port.DataReceived += new SerialDataReceivedEventHandler(comDataReceived);
+                Port1.DataReceived += new SerialDataReceivedEventHandler(comDataReceived1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void comDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+         
+            Input_Data((sender as SerialPort).ReadExisting());
+
+        }
+        private void comDataReceived1(object sender, SerialDataReceivedEventArgs e)
+        {
+           
+            //msg = Port1.ReadExisting(); 
+            Input_Data((sender as SerialPort).ReadExisting());
+
+        }
+        private void Input_Data(string _inval)
+        {
+          
+            //string indata = _inval.TrimEnd();
+            string sn = _inval.Replace(Environment.NewLine, "");
+            OleDbCommand myOleDbCommand = myOleDbConnection.CreateCommand();
+            //OleDbCommand query = myOleDbConnection.CreateCommand();
+            OleDbCommand query_stock = myOleDbConnectionStock.CreateCommand();
+            //myOleDbConnection.Open();
+            myOleDbCommand.CommandText = "select out_line_time, mo_number, model_name from sfism4.r_wip_tracking_t where serial_number = '" + sn + "'";
+           // query.CommandText = "select serial_number, mo_number, model_name from sfism4.r_wip_tracking_t where serial_number = '" + sn + "'";
+            OleDbDataReader myOleDbDataReader = myOleDbCommand.ExecuteReader();
+
+            if (myOleDbDataReader.HasRows)
+            {
+                myOleDbDataReader.Read();
+                var model_mame = myOleDbDataReader["model_name"] + string.Empty;
+                var out_line_time = myOleDbDataReader["out_line_time"] + string.Empty;
+                //myOleDbConnectionStock.Open();
+                query_stock.CommandText = "select PalletQty, CustomerNo, ProductName, ProductCode from tpvstock.dbo.products where SKDPartNo = '" + model_mame + "'";
+                OleDbDataReader myOleDbDataReaderStock = query_stock.ExecuteReader();
+                myOleDbDataReaderStock.Read();
+                var pallet_qty = myOleDbDataReaderStock["PalletQty"] + string.Empty;
+                var CustomerNo = myOleDbDataReaderStock["CustomerNo"] + string.Empty;
+                var ProductName = myOleDbDataReaderStock["ProductName"] + string.Empty;
+                var ProductCode = myOleDbDataReaderStock["ProductCode"] + string.Empty;
+                myOleDbDataReaderStock.Close();
+                //myOleDbConnectionStock.Close();
+               
+                SetText1(pallet_qty.ToString());
+                if (out_line_time == "")
+                {
+                    dataGridView1.Rows[x].Cells["SerialNumber"].Value = "";
+                 
+                    _msg = "SN: " + sn + " is not Route End";
+                    SetText(_msg.ToString());
+                    myOleDbDataReader.Close();
+                    //myOleDbConnection.Close();
+                }
+                else
+                {
+                    var mo_number = myOleDbDataReader["mo_number"] + string.Empty;
+                    if (InvokeRequired)
+                    {
+                        Invoke(new EventHandler(delegate
+                        {
+                            x = dataGridView1.Rows.Add(sn);
+                            dataGridView1.Rows[x].Cells["OrderNo"].Value = mo_number;
+                            //OleDbDataAdapter da = new OleDbDataAdapter();
+                            //da = new OleDbDataAdapter(query);
+                            //DataTable dt = new DataTable();
+                            //da.Fill(dt);
+                            //GridControl DG = new GridControl();
+                            //DG = this.gridControl1;
+                            //DG.DataSource = dt;
+                            _msg = "SN: " + sn + " OK";
+                            SetText(_msg.ToString());
+                            myOleDbDataReader.Close();
+                            //myOleDbConnection.Close();
+                            lb_pos.Text = "Кол-во:" + Convert.ToString (x + 1);
+                            if (x + 1 == Convert.ToInt64 (pallet_qty))
+                            {
+
+                            }
+
+                        }));
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                dataGridView1.Rows[x].Cells["SerialNumber"].Value = "";
+            
+                _msg = "NO SN";
+                SetText(_msg.ToString());
+                myOleDbDataReader.Close();
+                //myOleDbConnection.Close();
+            }
+
+
+        }
+
+        private void Input_Data_enter(string _inval, int x)
+        {
+           
+            //string indata = _inval.TrimEnd();
+            string sn = _inval.Replace(Environment.NewLine, "");
+            OleDbCommand myOleDbCommand = myOleDbConnection.CreateCommand();
+            //OleDbCommand query = myOleDbConnection.CreateCommand();
+            OleDbCommand query_stock = myOleDbConnectionStock.CreateCommand();
+            //myOleDbConnection.Open();
+            myOleDbCommand.CommandText = "select out_line_time, mo_number, model_name from sfism4.r_wip_tracking_t where serial_number = '" + sn + "'";
+            // query.CommandText = "select serial_number, mo_number, model_name from sfism4.r_wip_tracking_t where serial_number = '" + sn + "'";
+            OleDbDataReader myOleDbDataReader = myOleDbCommand.ExecuteReader();
+
+            if (myOleDbDataReader.HasRows)
+            {
+                myOleDbDataReader.Read();
+                var model_mame = myOleDbDataReader["model_name"] + string.Empty;
+                var out_line_time = myOleDbDataReader["out_line_time"] + string.Empty;
+                //myOleDbConnectionStock.Open();
+                query_stock.CommandText = "select PalletQty, CustomerNo, ProductName, ProductCode from tpvstock.dbo.products where SKDPartNo = '" + model_mame + "'";
+                OleDbDataReader myOleDbDataReaderStock = query_stock.ExecuteReader();
+                myOleDbDataReaderStock.Read();
+                var pallet_qty = myOleDbDataReaderStock["PalletQty"] + string.Empty;
+                var CustomerNo = myOleDbDataReaderStock["CustomerNo"] + string.Empty;
+                var ProductName = myOleDbDataReaderStock["ProductName"] + string.Empty;
+                var ProductCode = myOleDbDataReaderStock["ProductCode"] + string.Empty;
+                myOleDbDataReaderStock.Close();
+                //myOleDbConnectionStock.Close();
+                tb_palletqty.Text = pallet_qty;
+                //SetText1(pallet_qty.ToString());
+                if (out_line_time == "")
+                {
+                    dataGridView1.Rows[x].Cells["SerialNumber"].Value = "";
+                  
+                    _msg = "SN: " + sn + " is not Route End";
+                    tblog2.Text = _msg;
+                   
+                    myOleDbDataReader.Close();
+                    //myOleDbConnection.Close();
+                }
+                else
+                {
+                    var mo_number = myOleDbDataReader["mo_number"] + string.Empty;
+
+                    //int x = dataGridView1.Rows.Add(sn);
+                    dataGridView1.Rows[x].Cells["OrderNo"].Value = mo_number;
+                    //OleDbDataAdapter da = new OleDbDataAdapter();
+                    //da = new OleDbDataAdapter(query);
+                    //DataTable dt = new DataTable();
+                    //da.Fill(dt);
+                    //GridControl DG = new GridControl();
+                    //DG = this.gridControl1;
+                    //DG.DataSource = dt;
+                    _msg = "SN: " + sn + " OK";
+                    tblog2.Text = _msg;
+                    //SetText(_msg.ToString());
+                    myOleDbDataReader.Close();
+                    //myOleDbConnection.Close();
+                    lb_pos.Text = "Кол-во:" + Convert.ToString(x + 1);
+                    if (x + 1 == Convert.ToInt64(pallet_qty))
+                    {
+
+                    }
+
+                  
+                }
+            }
+            else
+            {
+                dataGridView1.Rows[x].Cells["SerialNumber"].Value = "";
+         
+                _msg = "NO SN";
+                tblog2.Text = _msg;
+              
+                myOleDbDataReader.Close();
+                //myOleDbConnection.Close();
+            }
+
+
+        }
+
+        private void openport_Click(object sender, EventArgs e)
+        {
+            comportmanager();
+
+        }
+        private void SetText(string text)
+        {
+           
+          
+                if (this.tblog2.InvokeRequired)
+                {
+                    SetTextCallback d = new SetTextCallback(SetText);
+                    this.Invoke(d, new object[] { text });
+                }
+                else
+                {
+                    this.tblog2.Text = text;
+                }
+            
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (dataGridView1[e.ColumnIndex, e.RowIndex].Value != null)
+                {
+                    x = e.RowIndex;
+                    SN = Convert.ToString (dataGridView1.CurrentCell.Value);
+
+                    Input_Data_enter(SN, x);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //myOleDbConnection.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void bt_conn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_refresh_Click(object sender, EventArgs e)
+        {
+            
+            SetPortNameValues(cbport);
+            SetPortNameValues(cbport1);
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    //int x = dataGridView1.Rows.Add("12");
+                    dataGridView1.CurrentCell.Value = SN;
+                    Input_Data(SN);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //myOleDbConnection.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SetText1(string text1)
+        {
+        
+            {
+                if (this.tb_palletqty.InvokeRequired)
+                {
+                    SetTextCallback st = new SetTextCallback(SetText1);
+                    this.Invoke(st, new object[] { text1 });
+                }
+                else
+                {
+                    this.tb_palletqty.Text = text1;
+                }
+            }
+        }
+    }
+}
